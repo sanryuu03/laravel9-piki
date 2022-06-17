fe20635c5820873ec1faa0d1796e0079019a6725
sanryuu03 <ryuuosku@gmail.com>
2022-06-17 14:32:30 +0700

    Horizontal Scroll Menu Community Partners

--- resources/views/admin/communitypartners.blade.php
+++ resources/views/admin/communitypartners.blade.php
@@ -44,7 +44,7 @@
   <!-- Header End-->
 
   .<div class="container-fluid">
-      <div class="card-body">
+      <div class="card-body table-responsive">
           <table class="table table-bordered table-striped">
               <thead>
                   <tr>
