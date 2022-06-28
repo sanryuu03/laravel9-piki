75ad55aa64491c08bddd57d0c611fad91696c27a
sanryuu03 <ryuuosku@gmail.com>
2022-06-28 10:25:49 +0700

    schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,6 +16,8 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->command('artisan cache:clear')->everyFiveMinutes();
+        $schedule->command('artisan view:clear')->everyFiveMinutes();
     }
 
     /**
